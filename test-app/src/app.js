0f70fec1b1257fc718d37f748c426895654eee0b
Travis Wood <travis_cw@yahoo.com>
2017-06-18 19:59:29 -0700

    Change root div's id to top_sales so can be targeted unambiguously by jQuery

--- test-app/src/app.js
+++ test-app/src/app.js
@@ -5,7 +5,6 @@ import TopSalesList from '../../dist/TopSalesList/index';
 import transformData from '../../dist/Utils/transformData';
 
 request.get('http://localhost:3000/PurchaseOrders', function (error, response, body) {
-  // console.log('body:', body);
   const topTen = transformData(JSON.parse(body));
-  ReactDOM.render(<TopSalesList topTen={topTen} />, document.getElementById('app'));
+  ReactDOM.render(<TopSalesList topTen={topTen} />, document.getElementById('top-sales'));
 });
